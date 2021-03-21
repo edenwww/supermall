1eaaeb71dddd56577a7b7f7a8e70a1af2198a48e
songsongp <782276256@qq.com>
2021-03-21 09:39:46 +0800

    回到顶部，无法滚动问题修复

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,39 +1,49 @@
 <template>
-  <div id="home">
+  <div id="home" class="wrapper">
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
-    <home-swiper :banners="banners"/>
-    <recommend-view :recommends="recommends"/>
-    <feature-view/>
-    <tab-control class="tab-controll"
-                 :titles="['流行','新款','精选']"
-                  @tabClick="tabClick"/>
-    <goods-list :goods="showGoods"/>
-
-
+    <scroll class="content"
+            ref="scroll"
+            :probe-type="3"
+            @scroll="contentScroll"
+            :pull-up-load="true"
+            @pullingUp="loadMore">
+      <home-swiper :banners="banners"/>
+      <recommend-view :recommends="recommends"/>
+      <feature-view/>
+      <tab-control class="tab-control"
+                   :titles="['流行','新款','精选']"
+                    @tabClick="tabClick"/>
+      <goods-list :goods="showGoods"/>
+    </scroll>
+    <!--组件内部触发点击事件-->
+    <back-top @click.native="backClick" v-show="isShowBackTop"/>
   </div>
 </template>
 
 <script>
-  import NavBar from "components/common/navbar/NavBar";
   import HomeSwiper from "./childComps/HomeSwiper";
   import RecommendView from "./childComps/RecommendView";
   import FeatureView from "./childComps/FeatureView";
 
-
+  import NavBar from "components/common/navbar/NavBar";
   import TabControl from "components/content/tabControl/TabControl";
+  import GoodsList from "components/content/goods/GoodsList";
+  import Scroll from "components/common/scroll/Scroll";
+  import BackTop from "components/content/backTop/BackTop";
 
   import {getHomeMultidata,getHomeGoods} from "network/home";
-  import GoodsList from "../../components/content/goods/GoodsList";
 
     export default {
       name: "Home",
       components:{
-        GoodsList,
-        TabControl,
-        NavBar,
         HomeSwiper,
         RecommendView,
-        FeatureView
+        FeatureView,
+        NavBar,
+        TabControl,
+        GoodsList,
+        Scroll,
+        BackTop,
       },
       data(){
         return {
@@ -45,6 +55,7 @@
             'sell':{page:0,list:[]},
           },
           currentType:'pop',
+          isShowBackTop:false
         }
       },
       computed:{
@@ -59,7 +70,6 @@
         this.getHomeGoods('pop')
         this.getHomeGoods('new')
         this.getHomeGoods('sell')
-
       },
       methods:{
         /**
@@ -78,6 +88,15 @@
               break
           }
         },
+        backClick(){
+          this.$refs.scroll.scrollTo(0,0)
+        },
+        contentScroll(position){
+          this.isShowBackTop = (-position.y) > 1000
+        },
+        loadMore(){
+          this.getHomeGoods(this.currentType)
+        },
         /**
          * 网络请求相关的方法
          */
@@ -90,12 +109,15 @@
           })
         },
         getHomeGoods(type){
+        	// 只获取页码，goods[type]页码没有变
           const page = this.goods[type].page + 1
           getHomeGoods(type,page).then(res => {
             // console.log(res)
             this.goods[type].list.push(...res.data.list)
+            // 改变页码
             this.goods[type].page += 1
 
+            this.$refs.scroll.finishPullUp()
           })
         }
       }
@@ -103,8 +125,43 @@
 </script>
 
 <style scoped>
-  .home-nav{
-    background-color: var(--color-tint);
-    color:#fff;
+  #home {
+    /*padding-top: 44px;*/
+    /*需要设置滚动高度，设置为可视窗口高度*/
+    height: 100vh;
+    position: relative;
   }
+
+  .home-nav {
+    background-color: var(--color-tint);
+    color: #fff;
+
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    z-index: 9;
+  }
+
+  .tab-control {
+    position: sticky;
+    top: 44px;
+    z-index: 9;
+  }
+
+  .content {
+    overflow: hidden;
+
+    position: absolute;
+    top: 44px;
+    bottom: 49px;
+    left: 0;
+    right: 0;
+  }
+
+  /*.content {*/
+  /*height: calc(100% - 93px);*/
+  /*overflow: hidden;*/
+  /*margin-top: 44px;*/
+  /*}*/
 </style>
